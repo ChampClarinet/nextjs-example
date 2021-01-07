4f418af50d8f84918bdcbd5c29e563e909c8286f
champclarinet <champbassist159@gmail.com>
2021-01-08 03:43:46 +0700

    api request part example

--- pages/api/random-number.ts
+++ pages/api/random-number.ts
@@ -1,5 +1,9 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
+    // console.log('request body is', req.query);
+    res.setHeader('X-Custom-Header', 'This is custom header');
+    res.setHeader('Set-Cookie', 'programmer?=true')
+    res.statusCode = 202;
     res.json({ number: Math.floor(Math.random() * 10) });
 }
